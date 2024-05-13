2456adde5ce7ed86aa65f75202b03219f015acfa
Shantanu9799 <cshantanu712@gmail.com>
2024-05-13 11:24:56 +0530

    issue solved

--- BoyOrGirl.cpp
+++ BoyOrGirl.cpp
@@ -4,13 +4,13 @@ using namespace std;
 string determineGender(string s) {
     int freq[26] = {0};
     for(int i = 0; i < s.size(); ++i) {
-        freq[s[i] - 'a']++;
+        freq[s[i] - 'a'] = 1;
     }
     int cnt = 0;
     for(int i = 0; i < 26; ++i) {
         if(freq[i] == 1) cnt++;
     }
-    return ((cnt & 1) == 1) ? "IGNORE HIM" : "CHAT WITH HER";
+    return ((cnt & 1) == 1) ? "IGNORE HIM!" : "CHAT WITH HER!";
 }
 
 int main() {
